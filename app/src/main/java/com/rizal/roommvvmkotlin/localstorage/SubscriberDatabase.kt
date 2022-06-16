557f5063d11dbb12a0ddeed2bbae14c8a8779e9b
rizhalmaulana <4s3tk1t4@gmail.com>
2022-06-17 01:29:50 +0700

    Setup room database

---
+++ app/src/main/java/com/rizal/roommvvmkotlin/localstorage/SubscriberDatabase.kt
@@ -0,0 +1,34 @@
+package com.rizal.roommvvmkotlin.localstorage
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Entity
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Subscriber::class], version = 1)
+abstract class SubscriberDatabase: RoomDatabase() {
+
+    abstract val subscriberDao: SubscriberDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: SubscriberDatabase? = null
+
+        fun getInstance(context: Context): SubscriberDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SubscriberDatabase::class.java,
+                        "subscriber_data_database"
+                    ).build()
+                }
+                return instance
+            }
+        }
+
+    }
+
+}
